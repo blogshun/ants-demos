8a4756575f76b46e1b2784553dd3569026f7ceea
雨中的旅行者 <530774513@qq.com>
2017-12-27 20:10:50 +0800

    修改Cookie

--- src/main/java/com/ants/demos/DemosApplication.java
+++ src/main/java/com/ants/demos/DemosApplication.java
@@ -5,7 +5,15 @@ import com.ants.common.annotation.boot.PropertyConfiguration;
 import com.ants.common.annotation.plugin.EnableEhcachePlugin;
 import com.ants.common.annotation.plugin.EnableRedisPlugin;
 import com.ants.common.annotation.service.Application;
+import com.ants.common.bean.Prop;
+import com.ants.core.context.AntsTool;
 import com.ants.core.startup.JTomcat;
+import com.ants.plugin.cache.EhCacheTpl;
+import com.ants.plugin.cache.RedisTpl;
+import com.ants.plugin.jms.ActiveMqTpl;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
 
 /**
  * Tomcat 方式启动
@@ -20,7 +28,9 @@ import com.ants.core.startup.JTomcat;
 @EnableRedisPlugin
 public class DemosApplication {
 
-    public static void main(String[] args) {
-        JTomcat.run(DemosApplication.class, 80);
+    public static void main(String[] args) throws InterruptedException {
+       // JTomcat.run(DemosApplication.class, 80);
+        Prop.use("application.properties");
+        AntsTool.getAliOss("http://ants-m.oss-cn-beijing.aliyuncs.com", "LTAIGtncqJ1CaDvW", "z6RtuvVSFqcCnn3rsYT6z4L9RcwWZH");
     }
 }
